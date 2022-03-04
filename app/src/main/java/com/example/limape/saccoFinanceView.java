4e25e3ba16d81d642b17842e42c536b2760dcc70
Diana Najjuma <diananajjuma92@gmail.com>
2022-03-04 13:34:45 +0300

    tired

---
+++ app/src/main/java/com/example/limape/saccoFinanceView.java
@@ -0,0 +1,15 @@
+package com.example.limape;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class saccoFinanceView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sacco_finance_view);
+
+    }
+}
